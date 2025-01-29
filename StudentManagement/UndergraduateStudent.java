8afb97c3e18f90bec59d8ff270858a5c3bc77eeb
Fayz A <faysalabdulkadir16@gmail.com>
2025-01-29 13:08:48 -0500

    Create UndergraduateStudent.java

---
+++ StudentManagement/UndergraduateStudent.java
@@ -0,0 +1,19 @@
+public class UndergraduateStudent extends Student
+{
+    public static final double UNDERGRAD_TUITION = 4000;
+	  public static final String UND_CLASSIFY = "Undergraduate";
+    public UndergraduateStudent(int pID, String pName)
+    {
+		super(pID, pName);
+        setTuition();
+        setClassification();
+    }
+    public void setTuition()
+    {
+        tuition = UNDERGRAD_TUITION;
+    }
+    public void setClassification()
+    {
+        classification = UND_CLASSIFY;
+    }
+}

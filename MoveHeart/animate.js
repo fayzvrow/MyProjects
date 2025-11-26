6bfaf7496e2feef889d2af9cc9986ce71692624e
Fayz A <faysalabdulkadir16@gmail.com>
2025-11-26 09:16:55 -0500

    Add files via upload

---
+++ MoveHeart/animate.js
@@ -0,0 +1,57 @@
+let timerId = null;
+
+window.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("click", startAnimation);
+});
+
+function startAnimation(e) {
+    let clickX = e.clientX;
+    let clickY = e.clientY;
+
+    if (timerId !== null) {
+        clearInterval(timerId);
+        timerId = null;
+    }
+
+    const img = document.querySelector("img");
+    if (img) {
+        img.classList.add("beat", "blink", "moving-shadow");
+    }
+
+    timerId = setInterval(function() {
+        moveImage(clickX, clickY);
+    }, 10);
+}
+
+function moveImage(x, y) {
+    const img = document.querySelector("img");
+    if (!img) return;
+
+    let imgX = parseInt(img.style.left);
+    let imgY = parseInt(img.style.top);
+
+    const centerX = Math.round(x - (img.width / 2));
+    const centerY = Math.round(y - (img.height / 2));
+
+    if (imgX === centerX && imgY === centerY) {
+        clearInterval(timerId);
+        timerId = null;
+        img.classList.remove("beat", "blink", "moving-shadow");
+        return;
+    }
+
+    if (imgX < centerX) {
+        imgX++;
+    }
+    else if (imgX > centerX) {
+        imgX--;
+    }
+    if (imgY < centerY) {
+        imgY++;
+    }
+    else if (imgY > centerY) {
+        imgY--;
+    }
+    img.style.left = imgX + "px";
+    img.style.top = imgY + "px";
+}
